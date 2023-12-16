daff9d05e654907074cd15730e7543fba4b38753
Debasish barai <debasishbaraiju@gmail.com>
2023-12-16 12:57:12 +0530

    initial commit

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+
+  -- to make christoomey work, Lazyvims default setting for <C-hjkl> to move between windows is to be remapped to christoomey functions. The same is done by adding the lines 39 to 42 in the keymaps.lua file.'
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+
+  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+}
